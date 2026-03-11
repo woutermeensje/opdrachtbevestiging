2bf5b49154c4be6d3fab10284fc9683e469d628e
WouterMeens <w.m.meens@gmail.com>
2026-03-11 22:26:21 +0100

    update

--- config/kvk.php
+++ config/kvk.php
@@ -3,6 +3,6 @@
 return [
     'base_url' => env('KVK_API_BASE_URL', 'https://api.kvk.nl/test/api/v1'),
     'search_base_url' => env('KVK_SEARCH_API_BASE_URL', 'https://api.kvk.nl/test/api/v2'),
-    'api_key' => env('KVK_API_KEY', 'l7xx1f2691f2520d487b902f4e0b57a0b197'),
+    'api_key' => env('KVK_API_KEY'),
     'timeout' => (int) env('KVK_API_TIMEOUT', 10),
 ];
