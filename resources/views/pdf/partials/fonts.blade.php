e0a9a01360da3b365c00b0a25cdc4aa2ff1b225a
WouterMeens <w.m.meens@gmail.com>
2026-09-05 21:25:07 +0200

    updat

--- resources/views/pdf/partials/fonts.blade.php
+++ resources/views/pdf/partials/fonts.blade.php
@@ -1,57 +1,57 @@
 {{--
     Ingesloten lettertype voor de PDF's: Aptos (© Microsoft Corporation).
-    De .ttf-bestanden staan in storage/fonts/ en worden door dompdf
-    ingelezen via onderstaande @font-face-regels. dompdf heeft geen
+    De officiële .ttf-bestanden staan in public/fonts/aptos/ en worden door
+    dompdf ingelezen via onderstaande @font-face-regels. dompdf heeft geen
     internettoegang (enable_remote = false), dus de fonts moeten lokaal
-    aanwezig zijn.
+    aanwezig zijn. dompdf cachet de metrics in storage/fonts/.
 --}}
 <style>
     @font-face {
         font-family: 'Aptos';
         font-style: normal;
         font-weight: 300;
-        src: url('{{ storage_path('fonts/Aptos-Regular.ttf') }}') format('truetype');
+        src: url('{{ public_path('fonts/aptos/Aptos-Light.ttf') }}') format('truetype');
     }
 
     @font-face {
         font-family: 'Aptos';
         font-style: normal;
         font-weight: 400;
-        src: url('{{ storage_path('fonts/Aptos-Regular.ttf') }}') format('truetype');
+        src: url('{{ public_path('fonts/aptos/Aptos.ttf') }}') format('truetype');
     }
 
     @font-face {
         font-family: 'Aptos';
         font-style: normal;
         font-weight: 500;
-        src: url('{{ storage_path('fonts/Aptos-Regular.ttf') }}') format('truetype');
+        src: url('{{ public_path('fonts/aptos/Aptos.ttf') }}') format('truetype');
     }
 
     @font-face {
         font-family: 'Aptos';
         font-style: normal;
         font-weight: 600;
-        src: url('{{ storage_path('fonts/Aptos-Bold.ttf') }}') format('truetype');
+        src: url('{{ public_path('fonts/aptos/Aptos-SemiBold.ttf') }}') format('truetype');
     }
 
     @font-face {
         font-family: 'Aptos';
         font-style: normal;
         font-weight: 700;
-        src: url('{{ storage_path('fonts/Aptos-Bold.ttf') }}') format('truetype');
+        src: url('{{ public_path('fonts/aptos/Aptos-Bold.ttf') }}') format('truetype');
     }
 
     @font-face {
         font-family: 'Aptos';
         font-style: italic;
         font-weight: 400;
-        src: url('{{ storage_path('fonts/Aptos-Italic.ttf') }}') format('truetype');
+        src: url('{{ public_path('fonts/aptos/Aptos-Italic.ttf') }}') format('truetype');
     }
 
     @font-face {
         font-family: 'Aptos';
         font-style: italic;
         font-weight: 700;
-        src: url('{{ storage_path('fonts/Aptos-BoldItalic.ttf') }}') format('truetype');
+        src: url('{{ public_path('fonts/aptos/Aptos-Bold-Italic.ttf') }}') format('truetype');
     }
 </style>
